f27dd5b08fa4c099128ccdf34619239b756db3b5
Aaron Buitenwerf <aaron.buitenwerf@carleton.ca>
2022-05-16 21:33:38 -0400

    fixed local dev

--- server/sequelize_init.js
+++ server/sequelize_init.js
@@ -1,4 +1,3 @@
-const dbConfig = require("./config/db.config");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
